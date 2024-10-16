23138e978ad8eb56c3d257a643c976e0dc249557
Surya Limbu <32124078@student.uwl.ac.uk>
2024-10-16 02:08:50 +0100

    fix: CI/CD configuration

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -152,6 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # The absolute path to the directory where collectstatic will collect static files
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
