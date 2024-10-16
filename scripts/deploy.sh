e0198fc9ccf687ea952a5eb590c0a00292d1c91b
Surya Limbu <32124078@student.uwl.ac.uk>
2024-10-16 02:17:18 +0100

    fix: deployment script ordering

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,13 +8,13 @@ set -e
 # Optional: Add a logging statement to track script execution.
 echo "Starting deployment script."
 
-# Stop the existing docker containers
-echo "Stopping existing Docker containers..."
-docker-compose -f docker-compose.prod.yml down || { echo "Failed to stop containers."; exit 1; }
-
 # Pull the latest changes from the repository
 echo "Pulling latest changes from Git repository..."
 git pull origin develop || { echo "Git pull failed."; exit 1; }
+
+# Stop the existing docker containers
+echo "Stopping existing Docker containers..."
+docker-compose -f docker-compose.prod.yml down || { echo "Failed to stop containers."; exit 1; }
 
 # Build and run the docker containers
 echo "Building and starting Docker containers..."
